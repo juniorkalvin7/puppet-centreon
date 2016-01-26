c5ed30933355b032002895650fcfa8201dcf8c4e
Junior Alvin <juniorkalvin7@gmail.com>
2016-01-25 23:19:03 -0300

    Projeto Puppet Centreon

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,10 @@
 
-class centreon {
+class centreon (
+	$clapi_binaries,
+	$clapi_username,
+	$clapi_password,
+	$clapi_export_file,
+	$clapi_log_file,
+	$poller_name,
   include centreon::server, centreon::client, centreon::packages
 }
